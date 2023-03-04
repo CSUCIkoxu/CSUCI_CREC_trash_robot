44bbf2ce88e5a5372f48b3d15baf4b200c33210f
CSUCIkoxu <70290605+CSUCIkoxu@users.noreply.github.com>
2023-03-03 18:57:15 -0800

    Create ClassificationSegmentationAI.py
    Created the main code for the Segmentation and Classification AI

---
+++ ClassificationSegmentationAI.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+CSUCI Conservation Robotics and Engineering Club
+Trash Robot Trash Classification Segmentation AI
+
+@author: Christopher Chang
+"""
+
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
+import keras.layers as layers
+
