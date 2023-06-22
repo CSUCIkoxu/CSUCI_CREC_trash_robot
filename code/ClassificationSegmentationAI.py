Fixed dataX being the wrong dimensions
-Fixed an issue with separateChannels that caused the individual dataframe for each color channel to be flatten into one giant array as opposed to a 2d array

code/ClassificationSegmentationAI.py
@@ -76,7 +76,7 @@ def separateChannels(imgData):
     if not isinstance(imgData, np.ndarray):
         tempImg = np.array(imgData)
     
-    transposed = tempImg.transpose(2,0,1).reshape(3,-1)
+    transposed = tempImg.transpose(2,0,1)
     
     separatedImg = [pd.DataFrame(i) for i in transposed]
     